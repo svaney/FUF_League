bdd97be1364df88b846d0e2a680306b4d43c863c
Deo92 <deo.gochava@gmail.com>
2013-05-15 13:03:45 +0400

    test5
    კლასში დავამატე ხაზი.

--- src/web/LeagueManager.java
+++ src/web/LeagueManager.java
@@ -5,6 +5,7 @@ public class LeagueManager {
 	public LeagueManager() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Hello World :drug:");
+		System.out.println("Ruska & Data, where are you?> :D");
 	}
 
 }
