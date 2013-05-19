709e9a1fb3a121f3eae3f99c331854f78799d329
dlapi10 <dlapi10@freeuni.edu.ge>
2013-05-20 00:14:04 +0400

    დავამატე ორი ცარიელი კლასი და შევქმენი პეკიჯები

---
+++ src/model/LeagueManager.java
@@ -0,0 +1,11 @@
+package model;
+
+public class LeagueManager {
+
+	public LeagueManager() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hello World :drug:");
+		System.out.println("Ruska & Data, where are you?> :D");
+	}
+
+}
