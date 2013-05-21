b35f3fd5f1a6a9f3289ca357a1508e456ad301e9
gkire10 <gkire10@freeuni.edu.ge>
2013-05-21 13:57:20 +0400

    ვსიო კირესთანაც გაკეთდა

---
+++ src/model/mainclasses/testMain.java
@@ -0,0 +1,17 @@
+package model.mainclasses;
+
+/**
+ * ეს კლასი პროსტა ტესტირებისთვის იყოს რაღაც დავტესტე ჩემთან
+ * author: gkire10
+ * */
+public class testMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("dfsdfs");
+	}
+
+}
