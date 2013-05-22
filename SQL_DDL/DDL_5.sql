534432f6528594da4c388ae7b492a7df2cffd584
dlapi10 <dlapi10@freeuni.edu.ge>
2013-05-22 16:53:31 +0400

    გაუშვით მხოლოდ მას მერე რაც პირველი 4 გაშვებულია, თუ ფბ-ზე არ გინახავთ, ესაა მე-5 DDL-ის მეორე ვერსია, ანუ თუ წინა ვერსია გაუშვით, მაშინ შეგიძლიათ მთლიანად წაშალოთ ბაზა, და პირველი ოთხი დდლ გაუშვათ :D და მერე ეს.

--- SQL_DDL/DDL_5.sql
+++ SQL_DDL/DDL_5.sql
@@ -1,32 +1,33 @@
-﻿USE fuf_league;
+USE FUF_LEAGUE;
 
--- შლის სვეტებს "პოზიცია", "ფბ-გვერდი", "ფეხი", "რეიტინგი";
-ALTER TABLE `fuf_league`.`players` DROP COLUMN `def_position` , DROP COLUMN `FB_Page` , DROP COLUMN `foot` , DROP COLUMN `rating` ;
+alter table players drop column FB_Page;
 
+alter table persons add column FB_Page varchar(300);
 
--- ზედა "ბრძანებით" წაშლილ სვეტებს ამატებს პერსონის ცხრილში;
-ALTER TABLE `fuf_league`.`persons` ADD COLUMN `rating` VARCHAR(10) NULL DEFAULT NULL  AFTER `Special_atr` , ADD COLUMN `fb_page` VARCHAR(300) NULL DEFAULT NULL  AFTER `rating` , ADD COLUMN `position` VARCHAR(45) NULL DEFAULT NULL  AFTER `fb_page`, ADD COLUMN `foot` VARCHAR(8) NULL DEFAULT NULL  AFTER `position` ;
+alter table degrees modify column degree_name varchar(20);
 
+alter table schools modify column school_name varchar(30);
 
--- მოთამაშის ცხრილში ამატებს მოთამაშის მაისურის ნომერის სვეტს
-ALTER TABLE `fuf_league`.`players` ADD COLUMN `number` VARCHAR(3) NULL DEFAULT NULL COMMENT 'ნომერი გუნდში (თუ არ აქვს NULL)'  AFTER `mistake_factor` ;
+-- შეზღუდვები დავადე რომ ამ მონაცემებს მხოლოდ 1დან 100მდე მიენიჭოთ
+alter table players add check (speed between 1 and 100), 
+add check (Dribbling between 1 and 100),
+add check (Heading between 1 and 100),
+add check (Club_Loyalty between 1 and 100),
+add check (Diving between 1 and 100),
+add check (Durability between 1 and 100),
+add check (Shooting between 1 and 100),
+add check (work_rate between 1 and 100),
+add check (creativity between 1 and 100),
+add check (Fear_factor between 1 and 100),
+add check (Killer_instinct between 1 and 100),
+add check (tackling between 1 and 100),
+add check (vision between 1 and 100),
+add check (passing between 1 and 100),
+add check (penalty between 1 and 100),
+add check (penalty_Saving between 1 and 100),
+add check (Leadership between 1 and 100),
+add check (shoot_stopping between 1 and 100),
+add check (mistake_factor between 1 and 100);
 
-
--- რეიტინგის სვეტის ტიპი გადავაკეთე სტრინგად. ასე უფრო მოხერხებულია, ხოლო საჭირო მეთოდი მას "დაპარსავს".
-ALTER TABLE `fuf_league`.`persons` CHANGE COLUMN `rating` `rating` VARCHAR(10) NULL DEFAULT NULL COMMENT 'ხალხის მიერ მინიჭებული რეიტინგი; ტიპი გადავაკეთე სტრინგად, ხოლო მისი მომხმარებელი მეთოდი \"დაპარსავს\".'  , CHANGE COLUMN `fb_page` `fb_page` VARCHAR(300) NULL DEFAULT NULL COMMENT 'Facebook url.'  , CHANGE COLUMN `position` `position` VARCHAR(45) NULL DEFAULT NULL COMMENT 'მოთამაშის პოზიცია. თუ პერსონა არ არის მოთამაშე და არის მწვრთნელი, მაშინ ამ ატრიბუტს გამოტოვებს მომხმარებელი მეთოდი.'  ;
-
-
--- დავამატე შეზღუდვა პოზიციის სვეტის მნიშვნელობაზე (წინათ ჰქონდა, მაგრამ გადატანისას წაეშალა);
-ALTER TABLE `fuf_league`.`persons` 
-  ADD CONSTRAINT `persons_ibfk_3`
-  FOREIGN KEY (`position` )
-  REFERENCES `fuf_league`.`positions` (`position` )
-  ON DELETE RESTRICT
-  ON UPDATE RESTRICT
-, ADD INDEX `persons_ibfk_3_idx` (`position` ASC) ;
-
-
--- კურსის, წონისა და სიმაღლის სვეტების ტიპი შევცვალე სტრინგად. ასე უფრო მოხერხებულია, ხოლო საჭირო მეთოდი მას "დაპარსავს".
-ALTER TABLE `fuf_league`.`persons` CHANGE COLUMN `Uni_Cur_course` `Uni_Cur_course` CHAR(4) NULL DEFAULT NULL COMMENT 'კურსი, რომელზეც იმყოფება'  , CHANGE COLUMN `Weight` `Weight` CHAR(4) NULL DEFAULT NULL COMMENT 'კგ-ებში. ვინახავ სტრინგად, საჭიროების შემთხვევაში მეთოდი \"დაპარსავს\"'  , CHANGE COLUMN `Height` `Height` CHAR(4) NULL DEFAULT NULL COMMENT 'სმ-ებში. ვინახავ სტრინგად, საჭიროების შემთხვევაში მეთოდი \"დაპარსავს\"'  ;
 
 commit;
