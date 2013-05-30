7b30773f157383b641b21851c47f03ce088d7baa
svaney <svaney@svaney-PC>
2013-05-31 00:01:10 +0400

    დაახლოებით დავწერე interface, რამის დამატება/შეცვლა თუა საჭირო მითხარით. დავიწყებ შაბათ-კვირას იმპლიმენტაციას (და მანადმე მითხარით :)

--- src/model/mainclasses/Championship.java
+++ src/model/mainclasses/Championship.java
@@ -1,5 +1,15 @@
 package model.mainclasses;
 
+import java.util.ArrayList;
+
 public interface Championship {
+	public void addTeam(Team team);
+	public void addTeams(ArrayList<Team> teams);
+	public int getTeamCount();
+	public Team getTeam(String teamName);
+	public ArrayList<Team> getAllTeams();
+	public void createGroups();
+	public Group getGroup(String groupName);
+	public void endChamp();
 
 }
