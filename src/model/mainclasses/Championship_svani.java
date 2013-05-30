2af244008225d8fc333a4a84c0672d5ab0f60be0
svaney <svaney@svaney-PC>
2013-05-31 00:01:27 +0400

    :>

--- src/model/mainclasses/Championship_svani.java
+++ src/model/mainclasses/Championship_svani.java
@@ -1,5 +1,59 @@
 package model.mainclasses;
 
+import java.util.ArrayList;
+
 public class Championship_svani implements Championship{
+	public Championship_svani() {
+		
+	}
+
+	@Override
+	public void addTeam(Team team) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addTeams(ArrayList<Team> teams) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTeamCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Team getTeam(String teamName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Team> getAllTeams() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createGroups() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Group getGroup(String groupName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void endChamp() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
