75b2ab9aec0dd63bdac7a6f7f9fb0b48c3557db9
dlapi10 <dlapi10@freeuni.edu.ge>
2013-06-01 01:28:30 +0400

    MatchDB_DLAPI

--- src/model/dblayer/MatchDB_DLAPI.java
+++ src/model/dblayer/MatchDB_DLAPI.java
@@ -13,6 +13,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+
+import model.mainclasses.PenaltyShoot;
 
 public class MatchDB_DLAPI implements MatchDB{
 	// ბაზის პარამეტრები
@@ -37,6 +40,227 @@ public class MatchDB_DLAPI implements MatchDB{
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * გვეუბნება რომელი ტურია ჩემპიონატის
+	 * @param matchID გუნდის იდენტიფიკატორი
+	 * @return String ტიპის პარამეტრი - რაუნდი, ეტაპი
+	 */
+	@Override
+	public String getStageOfMatch(int matchID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * გვეუბნება მატჩს რამდენი ჰქონდა დამატებით დრო (თუ არ ჰქონდა მაშინ 0)
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return წთ-ებში გამოსახული დრო
+	 */
+	@Override
+	public int getExtraTime(int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს მატჩის ჩატარების თარიღს
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return String ტიპის თარიღი
+	 */
+	@Override
+	public String getMatchDate(int matchID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * აბრუნებს კონკრეტულ მატჩზე, კონკრეტული გუნდის განაცხადს, List-ის სახით
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @param teamID int ტიპის მაიდენტიფიცირებელი - გუნდის
+	 * @return List ტიპის ობიექტი სადაც ყრია Integer ტიპის მაიდენტიფიცირებელი კოდები თითოეული მოთამაშისთვის
+	 */
+	@Override
+	public List<Integer> getMatchPlayersForTeam(int matchID, int teamID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * აბრუნებს მატჩის მონაწილე ორი გუნდის ID-ებს მასივის სახით
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return int ტიპის ორელემენტიანი მასივი, რომელშიც მოცემულია მატჩის მონაწილე გუნდების ID-ები
+	 */
+	@Override
+	public int[] getMatchTeams(int matchID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * აბრუნებს მოცემულ მატჩზე მოცემული გუნდის კაპიტანს, თუ ასეთი არ არსებობს აბრუნებს -1_ს (ანუ default კაპიტანი ჰყავდათ)
+	 * @param teamID int ტიპის მაიდენტიფიცირებელი - გუნდის
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return აბრუნებს int ტიპის ცვლადს, თუ გუნდის კაპიტანი არ იყო default ანუ null არ იყო მაშინ აბრუნებს ID-ს 
+	 * წინააღმდეგ შემთხვევაში აბრუნებს -1_ს
+	 */
+	@Override
+	public int getCapForMatchTeam(int teamID, int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს მოცემული მატჩის შესახებ ინფორმაციას - რომელ ჩემპიონატზე ჩატარდა
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return int ტიპის იდენტიფიკატორი
+	 */
+	@Override
+	public int getChampionshipID(int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * კონკრეტულ მატჩზე გუნდის რომელმა წევრებმა მიიღეს ყვითელი ბარათები
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @param teamID int ტიპის მაიდენტიფიცირებელი - გუნდის
+	 * @return List ტიპის ობიექტი სადაც წერია მოთამაშეების ID-ები
+	 */
+	@Override
+	public List<Integer> getYellowsForTeamInMatch(int matchID, int teamID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * კონკრეტულ მატჩზე გუნდის რომელმა წევრებმა მიიღეს წითელი ბარათები
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @param teamID int ტიპის მაიდენტიფიცირებელი - გუნდის
+	 * @return List ტიპის ობიექტი სადაც წერია მოთამაშეების ID-ები
+	 */
+	@Override
+	public List<Integer> getRedsForTeamInMatch(int matchID, int teamID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * აბრუნებს მატჩზე დაწერილ რევიუს
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return სტრინგ ტიპის ობიექტი სადაც წერია ტექსტი.
+	 */
+	@Override
+	public String getReview(int matchID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * აბრუნებს ჰქონდა თუ არა მატჩს პენლების სერია
+	 * @param  matchID მატჩის იდენტიფიკატორი
+	 * @return char ტიპის ობიექტი - 'Y'(YES) ან  'N'(NO)
+	 */
+	@Override
+	public char hasPenalties(int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს იყო თუ არა პენლების სერია
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return true თუ იყო, false თუ არ იყო
+	 */
+	@Override
+	public boolean wasPenaltiesSerie(int matchID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * აბრუნებს კონკრეტულ მატჩზე გუნდის გატანილ გოლებს ძირითად დროში
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @param teamID გუნდის იდენტიფიკატორი
+	 * @return რაოდენობა ძირითად დროში გატანილი გოლების
+	 */
+	@Override
+	public int getScoreForTeamForFullTime(int matchID, int teamID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს იმ გოლის იდენტიფიკატორს, რომელიც გავიგე მე-N მატჩის განმავლობაში
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @param nth მერამდენე
+	 * @return გოლის იდენტიფიკატორი
+	 */
+	@Override
+	public int getGoalNthInRow(int matchID, int Nth) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს ამ გუნდის მიერ გატანილი გოლების რაოდენობას დამატებით დროში
+	 * @param teamID გუნდის იდენტიფიკატორი
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return გოლების რაოდენობა
+	 */
+	@Override
+	public int getExtraTimeGoalForTeam(int teamID, int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს გუნდის მიერ მატჩში - პირველ ტაიმში გატანილ გოლების რაოდენობას
+	 * @param teamID გუნდის იდენტიფიკატორი
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return გოლების რაოდენობა
+	 */
+	@Override
+	public int getScoreForTeamFirstHalf(int teamID, int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს გუნდის მიერ პენლების სერიის შედეგს
+	 * @param teamID გუნდის იდენტიფიკატორი
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return List სადაც წერია PenaltyShoot ტიპის ობიექტები.
+	 */
+	@Override
+	public List<PenaltyShoot> getTeamPenalties(int teamID, int matchID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * აბრუნებს რამდენი გაიტანა გუნდმა პენლების სერიაში
+	 * @param teamID გუნდის იდენტიფიკატორი
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return რაოდენობა
+	 */
+	@Override
+	public int getTeamsScoresInPenalties(int teamID, int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * აბრუნებს რამდენი ვერ გაიტანა გუნდმა პენლების სერიაში
+	 * @param teamID გუნდის იდენტიფიკატორი
+	 * @param matchID მატჩის იდენტიფიკატორი
+	 * @return რაოდენობა
+	 */
+	@Override
+	public int getTeamsMissedInPenalties(int teamID, int matchID) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 	
